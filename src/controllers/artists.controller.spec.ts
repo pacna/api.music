6fddc4eb65ffe7e935051053c57a6c2c3c7e9dc9
pacna <paulc1231@gmail.com>
2019-12-27 19:01:49 -0500

    Add unit tests

---
+++ src/controllers/artists.controller.spec.ts
@@ -0,0 +1,60 @@
+// controllers
+import { ArtistsController } from './artists.controller';
+
+// services
+import { ArtistsService } from '../services/artists.service';
+
+// interfaces
+import { IArtists } from '../interfaces/artists.interface';
+
+// others
+import { Model } from 'mongoose';
+
+const artistsMockResponse = [
+    {
+        artist: "Hello World",
+        albums:[
+            {
+                title: "Blah Album",
+                songs: [
+                    {
+                        "title": "Blah",
+                        "path": "https://www.w3schools.com/tags/horse.mp3"
+                    }
+                ]
+            }
+        ]       
+    }
+]
+
+class ArtistsMockService {
+    async getArtists(): Promise<any> {
+        return artistsMockResponse; 
+    }
+}
+
+describe('SongssController', () => {
+  let artistsController: ArtistsController;
+  let artistsService: ArtistsService;
+  let artistsModel: Model<IArtists>
+
+  beforeEach(() => {
+    artistsService = new ArtistsService(artistsModel);
+    artistsController = new ArtistsController(artistsService);
+  });
+
+  describe('songs', () => {
+    it('should retrieve artists', async () => {
+        // ARRANGE
+        const artistsMockService = new ArtistsMockService();
+        jest.spyOn(artistsService, 'getArtists').mockImplementation(artistsMockService.getArtists);
+
+        // ACT
+        const response = await artistsController.getArtists();
+
+        // ASSERT
+        expect(artistsService.getArtists).toBeCalled();
+        expect(response).toBe(artistsMockResponse);
+    });
+  });
+});
---
+++ src/controllers/songs.controller.spec.ts
@@ -0,0 +1,109 @@
+// controllers
+import { SongsController } from './songs.controller';
+
+// services
+import { SongsService } from '../services/songs.service';
+
+// interfaces
+import { ISongs } from '../interfaces/songs.interface';
+import { IFavorite } from '../interfaces/favorite.interface';
+
+// others
+import { Model } from 'mongoose';
+
+const songsMockResponse = [
+    {
+        _id: '123',
+        title: "Blah",
+        artist: "Hello World",
+        length: "3:39",
+        path: "https://www.w3schools.com/tags/horse.mp3",
+        favorite: false
+    },
+    {
+        _id: '456',
+        title: "Blah Blah",
+        artist: "Foo Bar",
+        length: "3:33",
+        path: "https://www.w3schools.com/tags/horse.mp3",
+        favorite: true
+    }
+]
+
+class SongsMockService {
+    async getSongs(): Promise<any> {
+        return songsMockResponse; 
+    }
+    async getFavorites(): Promise<any> {
+        return songsMockResponse.filter((songs) => songs.favorite === true);
+    }
+    async updateFavorite(id: string, body: IFavorite): Promise<void> {
+        const song = songsMockResponse.find((songs) => songs._id === id);
+        song.favorite = body.favorite;
+    }
+}
+
+describe('SongssController', () => {
+  let songsController: SongsController;
+  let songsService: SongsService;
+  let songsModel: Model<ISongs>
+
+  beforeEach(() => {
+    songsService = new SongsService(songsModel);
+    songsController = new SongsController(songsService);
+  });
+
+  describe('songs', () => {
+    it('should retrieve songs', async () => {
+        // ARRANGE
+        const songsMockService = new SongsMockService();
+        jest.spyOn(songsService, 'getSongs').mockImplementation(songsMockService.getSongs);
+
+        // ACT
+        const response = await songsController.getSongs();
+
+        // ASSERT
+        expect(songsService.getSongs).toBeCalled();
+        expect(response).toBe(songsMockResponse);
+    });
+
+    it('should retrieve favorite songs', async() => {
+        // ARRANGE
+        const songsMockService = new SongsMockService();
+        jest.spyOn(songsService, 'getFavorites').mockImplementation(songsMockService.getFavorites);
+
+        // ACT
+        const response = await songsController.getFavorites();
+
+        // ASSERT
+        expect(songsService.getFavorites).toHaveBeenCalled();
+        expect(response[0].favorite).toBe(true);
+    })
+
+    it('should add a favorite', async() => {
+        // ARRANGE
+        const songsMockService = new SongsMockService();
+        jest.spyOn(songsService, 'updateFavorite').mockImplementation(songsMockService.updateFavorite);
+
+        // ACT
+        await songsController.updateFavorite('123', {favorite: true});
+
+        // ASSERT
+        expect(songsService.updateFavorite).toHaveBeenCalled();
+        expect(songsMockResponse[0].favorite).toBe(true);
+    })
+
+    it('should remove a favorite', async() => {
+        // ARRANGE
+        const songsMockService = new SongsMockService();
+        jest.spyOn(songsService, 'updateFavorite').mockImplementation(songsMockService.updateFavorite);
+
+        // ACT
+        await songsController.updateFavorite('123', {favorite: false});
+
+        // ASSERT
+        expect(songsService.updateFavorite).toHaveBeenCalled();
+        expect(songsMockResponse[0].favorite).toBe(false);
+    })
+  });
+});
